8eda65bc50ecccbc6507cea0a9a7a9e1e6cf0e91
devamcodes <devamagrawal.codes@gmail.com>
2020-07-25 02:13:57 +0530

    spelling improved.

--- milestone_1.py
+++ milestone_1.py
@@ -2,7 +2,7 @@
 Project:Make a movie collection app.
     *Debug(program termination)
 Author:Devam A
-Discreption:
+Description:
 this program take input for movies and makes the list of them and then prints out the list if invalid input is given then it shows error.
 this program lacks in saving the data when its finished due to use of list instead of database.
 Status:Failed
@@ -20,11 +20,16 @@ def find_movie(method):
                     return movies.index(element)
                 
 def menu_options():
-    menu_options = ['ADD A MOVIE','QUIT THE PROGRAM','FIND MOVIE','VIEW THE MOVIE LIST']
+    menu_options = ['ADD A MOVIE','VIEW THE MOVIE LIST','FIND MOVIE','QUIT PROGRAM']
     for _ in range(len(menu_options)):
         output = print(_, menu_options[_])
     return output
-
+def add_movie():
+    user_input_movies = input("PLEASE ENTER A MOVIE NAME:")
+    movies.append(user_input_movies)
+    user_input = int(input("PLEASE ENTER THE OPTION NUMBER:"))
+    output = print("THE MOVIE IS ADDED TO THE COLLECTION.")
+    return output
 
 user_conformation = int(input("TO CONTINUE TYPE 8 OR ENTER ANY NUMBER TO TERMINATE:"))
 
@@ -33,25 +38,22 @@ menu_options()
 while user_conformation == 8:
     user_input = int(input("PLEASE ENTER THE OPTION NUMBER:"))
     #TODO:afetr this step the program starts malfunctioning.
-    movies = [] 
+    movies = []
+    
     if user_input == 0:
-
         while user_input == 0:
-            user_input_movies = input("PLEASE ENTER A MOVIE NAME:")
-            movies.append(user_input_movies)
-            user_input = int(input("PLEASE ENTER THE OPTION NUMBER:"))
-
+                add_movie()
     elif user_input == 1:
-        print("THE PROGRAM TERMINATED.")
-
+         print(movies)
+        
     elif user_input == 2:
         print("ENTER MOVIE NAME TO FIND THE MOVIE:")
         find_movie
 
     elif user_input == 3:
-         print(movies)
-         menu_options()
-
+        print("THE PROGRAM TERMINATED.")
+        menu_options()
+    
     else:
         print("INVALID INPUT PLEASE TRY AGAIN:")
     
