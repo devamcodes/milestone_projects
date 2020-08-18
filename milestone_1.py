0894cbe75a84c429fb84818d7b5e0a05e32c721d
devamcodes <devamagrawal.codes@gmail.com>
2020-08-19 00:27:45 +0530

    too many changes but inshort program improved

--- milestone_1.py
+++ milestone_1.py
@@ -20,8 +20,8 @@ def enter_user_name():
     if user_name_booleane == True:
         print(f"Hello, {user_name.title()}")
     else:
-        input("program terminated!!")
-        raise ValueError("Invalid input please enter a valid name:")
+        print("program terminated!!")
+        raise ValueError("Invalid input please enter a valid name")
 
 
 
@@ -33,50 +33,61 @@ def find_movie_option():
     #To Do:1
     find_movie = input("ENTER MOVIE NAME:")
     if find_movie in movies:
-        print(f"We have found the movie in your movie collection:-> {find_movie.title()}")
+        print(f"BOOYA!!!We have found the {find_movie.title()} movie in your movie collection")
     else:
         raise ValueError("THIS MOVIE IS NOT IN YOUR COLLECTION,TRY AGAIN!")
 
+#TODO:after raising the error the program must continue but still it is not happening after the use of that keyword.
 
 
 #TODO:remove movie option remaining.
 menu_option = """
 Please enter the option number:
-  0. Add Movie
-  1. View MovieList
-  2. Find Movie
+  1. Add Movie
+  2. View MovieList
+  3. Find Movie
+  4. Remove Movie
+  5. Exit Program
 -->"""
 
 def add_movie():
     
     user_input_movies = input("PLEASE ENTER A MOVIE NAME:")
     movies.append(user_input_movies)
-    output = print("THE MOVIE IS ADDED TO THE COLLECTION.")
-    return output
+    print("THE MOVIE IS ADDED TO THE COLLECTION.")
+
+
+
+def remove_movie():
+    user_input_movie_name = input("enter the movie name you want to remove:")
+    if user_input_movie_name in movies:
+        print(f"we have removed  '{user_input_movie_name}' movie from your movie collection.")
+    else:
+        print(f"we are unable to find the movie in your movie collection...")
+        raise ValueError("please enter the correct movie name.")
 
 
 enter_user_name()
-user_conformation = int(input("TO CONTINUE TYPE 0 OR ENTER 1 TO TERMINATE THE ROGRAM:"))
-if user_conformation == 0:
-    while user_conformation == 0:
+user_input = 0
+if user_input == 0:
+    while user_input == 0:
         #TODO:if enter is pressed instead of any number then raise an error.
         user_input = int(input(menu_option))
-        if user_input == 0:
+        #TODO:if enter is pressed without any value then error should raise and loop should continue.
+        if user_input == 1:
             add_movie()
-
-        elif user_input == 1:
-            print(movies)
         elif user_input == 2:
+            print(movies)
+        elif user_input == 3:
             find_movie_option()
+        elif user_input == 4:
+            remove_movie()
+        elif user_input == 5:
+            print("program completed...")
+            break
         else:
-            print("INVALID INPUT PLEASE TRY AGAIN:")
+            raise ValueError("Invalid Input!! try again...")
+        user_input = 0
 
-        user_conformation = int(input("TO CONTINUE TYPE 0 OR ENTER 1 TO TERMINATE THE PROGRAM:"))
-    #TODO:after this user_conformation if input is somthing else than 1 it still completes the program instead of error.
-    else:
-        input("Thank You!! come again and have a nice day!! PRESS ENTER.")
-        print("PROGRAM COMPLETED.")
-elif user_conformation == 1:
-    print("program terminated by the user")
 else:
     raise ValueError("Invalid input. please try again..")
